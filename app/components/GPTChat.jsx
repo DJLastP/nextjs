487b1f5d0f266437516a61559a91658e3ad17143
DJLastP <DJLastP12345@gmail.com>
2025-03-25 17:11:04 +0700

    수정

--- app/components/GPTChat.jsx
+++ app/components/GPTChat.jsx
@@ -27,6 +27,9 @@ const GPTChat = () => {
 
         const response = await fetch('/api/chat', {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
                 messages: [
                     {
