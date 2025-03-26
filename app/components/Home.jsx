8ca94f746d0cc21f4ba45cb7df89b6b623ffa984
DJLastP <DJLastP12345@gmail.com>
2025-03-26 17:41:02 +0700

    텍스트

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
             </div>
             <div className={'text-center text-left md:text-right'}>
                 <h1 className={'text-4xl font-bold mb-2'}>윤지수</h1>
-                <p className="text-xl mb-4 dark:text-gray-100">풀스택 개발자</p>
+                <p className="text-xl mb-4 dark:text-gray-100">풀스택 개발자로 성장중인 백엔드 개발자</p>
                 <p className="text-gray-600 text-sm md:text-base dark:text-gray-300 leading-relaxed a">
-                    안녕하세요. 3년차 풀스택 개발자 <strong className="font-semibold text-gray-800 dark:text-white">윤지수</strong>입니다. <br/>
+                    안녕하세요. 3년차 백엔드 개발자 <strong className="font-semibold text-gray-800 dark:text-white">윤지수</strong>입니다. <br/>
                     React와 Spring 중심의 웹 서비스를 주로 개발해왔고, <br/>
                     Express, MongoDB 등 다양한 기술 스택도 함께 사용했습니다. <br/>
                     여러 프로젝트를 기획부터 배포까지 혼자서 완성한 경험이 있으며, <br/>
