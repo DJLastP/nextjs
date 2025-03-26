4e2bee28f727164dc5183a106fb6103a422e7f31
DJLastP <DJLastP12345@gmail.com>
2025-03-26 17:34:10 +0700

    footer 수정

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="text-center text-sm text-gray-500 py-8 space-y-1">
             <p className="text-sm text-gray-500 text-center py-8">
-                © 2025 윤지수 — Built with <span className="text-blue-500 font-medium">Next.js 14</span> & <span className="text-sky-400 font-medium">Tailwind CSS</span>
+                © 2025 윤지수 — Built with <span className="text-blue-500 font-medium">Next.js 15</span> & <span className="text-sky-400 font-medium">Tailwind CSS</span>
             </p>
         </footer>
 
