1ac94c579083d98f3a2b4b86159fa5b5f38566eb
DJLastP <DJLastP12345@gmail.com>
2025-07-03 19:33:57 +0900

    gaid 수정

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-    {/*<GoogleTagManager gtmId="G-V95031MM33" />*/}
-    <GoogleAnalytics gaId="G-V95031MM33" /> {/* GA4 ID 입력 */}
+    <GoogleAnalytics gaId="G-5JJPJXCW7V" /> {/* GA4 ID 입력 */}
     <body
       >
         {children}
